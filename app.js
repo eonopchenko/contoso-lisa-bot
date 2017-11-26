a89598db8e8f7ddca9f9b9aa5ab9cfc5e2ae5ad7
eonopchenko <jake.spb@gmail.com>
2017-11-27 10:42:52 +1300

    +Added parametrized balance request

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 var connector = new builder.ChatConnector({
     appId: "a0392848-aeed-498c-9a5b-00061fef7fa7",
     appPassword: "mktzCDB6234gjkRRJY7@}}]",
-    stateEndpoint: process.env.BotStateEndpoint,
-    openIdMetadata: process.env.BotOpenIdMetadata 
 });
 
 // Listen for messages from users 
@@ -30,26 +28,9 @@ server.post('/api/messages', connector.listen());
 * ---------------------------------------------------------------------------------------- */
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
-
-// Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
-
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
-
-// Main dialog with LUIS
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'!', session.message.text);
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
 });
-
-bot.dialog('/', intents);
 
 // This line will call the function in your luisDialog.js file
 luis.startDialog(bot);
--- controller/luisDialog.js
+++ controller/luisDialog.js
@@ -3,41 +3,57 @@ var request = require('request');
 
 exports.startDialog = function (bot) {
 
-    var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6dbfd07b-5564-4db1-9508-7f04933f66c3?subscription-key=81c2b5daa58b4f9fa8a5e7ff4d5591a7&timezoneOffset=0&q=');
+    var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/d3b35771-e7bd-46fb-83c3-d58c28897e26?subscription-key=81c2b5daa58b4f9fa8a5e7ff4d5591a7&timezoneOffset=0&q=');
 
     bot.recognizer(recognizer);
 
-    bot.dialog('GetBalance', function (session, args) {
-
-        request({
-            headers: {
-              'ZUMO-API-VERSION': '2.0.0',
-              'Content-Type': 'application/json'
-            },
-            uri: 'http://contoso-lisa-mobile.azurewebsites.net/tables/CustomerTable',
-            method: 'GET'
-          }, function (error, response, body) {
-            var success = false;
-            if(error != null) {
-              console.log('error:', error);
-              console.log('statusCode:', response && response.statusCode);
-              console.log('body:', body);
+    bot.dialog('GetBalance', [
+        function (session, args, next) {
+            session.dialogData.args = args || {};
+            if(!session.conversationData["username"]) {
+                builder.Prompts.text(session, 'It looks like we are not familiar yet. My name is Lisa. What is your name?');
             } else {
-              var customers = JSON.parse(body);
-              for(var index in customers) {
-                if(customers[index].username == "eugene") {
-                  success = true;
-                  session.send("Your balance is $" + customers[index].balance + ".");
-                  break;
-                }
-              }
+                next();
+            }
+        },
+        function (session, results, next) {
+            if (results.response) {
+                session.conversationData["username"] = results.response;
+                session.send("It's nice to meet you, " + session.conversationData["username"] + ".");
             }
 
-            if(!success) {
-                session.send("Sorry, no data found about your account. Do you want to open a new one?");
-            }
-          });
-    }).triggerAction({
+            session.send("Balance request in progress...");
+
+            request({
+                headers: {
+                    'ZUMO-API-VERSION': '2.0.0',
+                    'Content-Type': 'application/json'
+                },
+                uri: 'http://contoso-lisa-mobile.azurewebsites.net/tables/CustomerTable',
+                method: 'GET'
+            }, function (error, response, body) {
+                var success = false;
+                if(error != null) {
+                    console.log('error:', error);
+                    console.log('statusCode:', response && response.statusCode);
+                    console.log('body:', body);
+                } else {
+                    var username = session.conversationData["username"];
+                    var customers = JSON.parse(body);
+                    for(var index in customers) {
+                        if(customers[index].username == username) {
+                            success = true;
+                            session.send("Your balance is $" + customers[index].balance + ".");
+                            break;
+                        }
+                    }
+                }
+                if(!success) {
+                    session.send("Sorry, no data found about your account. Do you want to open a new one?");
+                }
+            });
+        }
+    ]).triggerAction({
         matches: 'GetBalance'
     });
 }
