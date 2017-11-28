4fbc7fbfabb843e7ba2597b77a2fb4ff82cc804b
eonopchenko <jake.spb@gmail.com>
2017-11-28 20:02:45 +1300

    Revert "~Test deploy"
    This reverts commit 82a8ec8c54ed370717224f40be5d3dc402be3f75.

--- app.js
+++ app.js
@@ -1,68 +1,60 @@
-// This loads the environment variables from the .env file
-require('dotenv-extended').load();
+/*-----------------------------------------------------------------------------
+A simple echo bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
 
-var builder = require('botbuilder');
 var restify = require('restify');
+var builder = require('botbuilder');
+var luis = require('./controller/luisDialog');
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+   console.log('%s listening to %s', server.name, server.url); 
 });
-
-// Create connector and listen for messages
+  
+// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: "a0392848-aeed-498c-9a5b-00061fef7fa7",
-    appPassword: "mktzCDB6234gjkRRJY7@}}]"
+    appPassword: "mktzCDB6234gjkRRJY7@}}]",
 });
+
+// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-var instructions = 'Welcome to the Bot to showcase the DirectLine API. Send \'Show me a hero card\' or \'Send me a BotFramework image\' to see how the DirectLine client supports custom channel data. Any other message will be echoed.';
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
 
+// Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector, function (session) {
 
-    var reply = new builder.Message()
-        .address(session.message.address);
+    if ((session.userData["username"] != null) && (session.userData["username"] != undefined) && (session.userData["username"] != "")) {
+        session.send('Welcome back, ' + session.userData["username"] + '.');
+    } else {
+        var welcomeCard = new builder.HeroCard(session)
+        .title('Welcome to Contoso Bank Chatbot')
+        .images([
+            new builder.CardImage(session)
+            .url('https://placeholdit.imgix.net/~text?txtsize=56&txt=Contoso%20Bank&w=640&h=330')
+        ]);
 
-    var text = session.message.text.toLocaleLowerCase();
-    switch (text) {
-        case 'show me a hero card':
-            reply.text('Sample message with a HeroCard attachment')
-                .addAttachment(new builder.HeroCard(session)
-                    .title('Sample Hero Card')
-                    .text('Displayed in the DirectLine client'));
-            break;
-
-        case 'send me a botframework image':
-            reply.text('Sample message with an Image attachment')
-                .addAttachment({
-                    contentUrl: 'https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png',
-                    contentType: 'image/png',
-                    name: 'BotFrameworkOverview.png'
-                });
-
-            break;
-
-        default:
-            reply.text('You said \'' + session.message.text + '\'');
-            break;
+        session.send(new builder.Message(session).addAttachment(welcomeCard));
+        session.send('Hi, my name is Lisa!');
     }
-
-    session.send(reply);
-
 });
 
-
-bot.on('conversationUpdate', function (activity) {
-    // when user joins conversation, send instructions
-    if (activity.membersAdded) {
-        activity.membersAdded.forEach(function (identity) {
-            if (identity.id === activity.address.bot.id) {
-                var reply = new builder.Message()
-                    .address(activity.address)
-                    .text(instructions);
-                bot.send(reply);
+// Send welcome when conversation with bot is started, by initiating the root dialog
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                bot.beginDialog(message.address, '/');
             }
         });
     }
 });
+
+// This line will call the function in your luisDialog.js file
+luis.startDialog(bot);
